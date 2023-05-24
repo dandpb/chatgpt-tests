fb297bb70102dc0bd5885af25cf0a41066dafe19
Daniel Barreto <daniel.barreto@novaescola.org.br>
2023-05-24 11:47:19 -0300

    leitura de metodos e documentaçao

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
+const fs = require('fs');
+const esprima = require('esprima');
+
 require("dotenv").config();
 
 
@@ -24,22 +27,64 @@ async function descreverCodigo(blocoCodigo) {
   return resposta.data.choices[0].text.trim();
 }
 
+// Função para encontrar e extrair nomes de métodos em um arquivo de código JavaScript
+function extrairMetodosArquivoJS(caminhoArquivo) {
+  const codigo = fs.readFileSync(caminhoArquivo, 'utf8');
+
+  // Analisar o código em uma estrutura de árvore sintática
+  const arvore = esprima.parseScript(codigo, { range: true });
+
+  // Objeto de hash para armazenar nome do método e seu conteúdo
+  const metodos = {};
+
+  // Percorrer a árvore sintática
+  percorrerArvore(arvore, (no) => {
+    if (no.type === 'FunctionDeclaration' || no.type === 'FunctionExpression' || no.type === 'ArrowFunctionExpression') {
+      const nomeMetodo = no.id.name || '[Anônimo]';
+      const conteudoMetodo = codigo.substring(no.range[0], no.range[1]);
+      metodos[nomeMetodo] = conteudoMetodo;
+    }
+  });
+
+  return metodos;
+}
+
+// Função auxiliar para percorrer a árvore sintática
+function percorrerArvore(no, callback) {
+  callback(no);
+  for (const chave in no) {
+    if (no.hasOwnProperty(chave)) {
+      const valor = no[chave];
+      if (typeof valor === 'object' && valor !== null) {
+        if (Array.isArray(valor)) {
+          valor.forEach((item) => percorrerArvore(item, callback));
+        } else {
+          percorrerArvore(valor, callback);
+        }
+      }
+    }
+  }
+}
+
+
   try {
         // Ler o arquivo de código JavaScript
-        const fs = require('fs');
         const arquivoJS = 'arquivo.js';
-        const codigoJS = fs.readFileSync(arquivoJS, 'utf8');
 
-        // Dividir o código em blocos
-        const blocos = codigoJS.split('\n\n');
+        // Dividir o código em metodos
+        const metodos = extrairMetodosArquivoJS(arquivoJS);
 
         // Processar cada bloco de código
         (async () => {
-          for (let i = 0; i < blocos.length; i++) {
-            const bloco = blocos[i];
-            const descricao = await descreverCodigo(bloco);
-            console.log(`Bloco ${i + 1}: ${descricao}\n`);
+
+          for (const chave in metodos) {
+            if (metodos.hasOwnProperty(chave)) {
+              const valor = metodos[chave];
+              const descricao = await descreverCodigo(valor);
+              console.log(`Chave: ${chave}, descriçao: ${descricao}`);
+            }
           }
+
         })();
 
   } catch (error) {
